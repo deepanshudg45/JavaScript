b8b16b7fbfccaeddc81ee20bf8e673fe08f9dc79
Deepanshu Gahatori <128813709+deepanshudg45@users.noreply.github.com>
2024-09-07 08:21:42 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score= 33
+// let score="33abc"
+// let score= null
+// let score= undefined
+// let score= true
+let score= "Deep"
+
+console.log(typeof score);
+//or
+console.log(typeof (score));
+
+let valueInNumber= Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33"-> 33
+// "33abc"-> NaN is actual value but type is number
+// true-> 1; false-> 0;
+
+// let isLoggedIn= 1
+// let isLoggedIn= 0
+let isLoggedIn= ""
+// let isLoggedIn= "Hitesh"
+let booleanLoggeedIn =Boolean(isLoggedIn)
+console.log(booleanLoggeedIn);
+
+// 1-> true; 0-> false;
+// ""-> false
+//"hitesh"-> true
