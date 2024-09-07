b0321fae5d9a094954640d0f90eac3480ed980ca
Deepanshu Gahatori <128813709+deepanshudg45@users.noreply.github.com>
2024-09-07 08:03:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS cose as newer version
+
+//alert(3+3) //we are using node.js not browser
+
+console.log(3
+    +
+    3)  //code readability should be high
+console.log("Hitesh")
+
+let name= "Deepanshu"
+let age= 20
+let isLoggedIn= false
+
+//Data Types
+
+//Primitive Data Types-
+
+// number-> 2to power 53
+// bigint
+// string -> " "
+// boolean -> true/false
+// null -> standalone value
+// undefined -> 
+// symbol -> unique 
+
+//objects (talk about this later)
+
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
