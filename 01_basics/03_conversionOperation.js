39da71e340fd44cdf9b11989bf462dffc3d74e58
Deepanshu Gahatori <128813709+deepanshudg45@users.noreply.github.com>
2024-09-07 08:53:22 +0000

    more about confusion in datatype conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,36 @@ console.log(booleanLoggeedIn);
 // 1-> true; 0-> false;
 // ""-> false
 //"hitesh"-> true
+
+
+//*****************************************************************Opertaions**********************************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="Deep"
+let str2= " Gahatori"
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(2+"1");
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+
+console.log(+true); //1 (convert true to 1)
+// console.log(true+); not possible
+console.log(+""); //0 (convert false to 1)
+
+let gameCounter=100
+++gameCounter; //prefix 101
+gameCounter++; //postfix increment 101
+console.log(gameCounter); // consider mdn to learn more about prefix and postfix
